b82b453d69036bd99fea252afac9a20e3e59eb59
David Puerto <me@davidpuerto.com>
2020-11-20 22:22:10 -0800

    updating the cart dropdown UX to allow to adjust quantity and remove items

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
 import './cart-item.styles.scss';
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+const CartItem = ({ item: { imageUrl, price, name, quantity }, item, clearItem, addItem, removeItem }) => (
     <div className='cart-item'>
         <img src={imageUrl} alt='item' />
         <div className='item-details'>
-            <span className='name'>{name}</span>
-            <span className='price'>
-                {quantity} x ${price}
-            </span>
+            <div className='name flex-row'>{name}</div>
+            <div className="flex-row">
+                <div className="arrow" style={{ marginRight: 10 }} onClick={() => removeItem(item)}>&#10094;</div>
+                <div className='price'>
+                    {quantity} x ${price}
+                </div>
+                <div className="arrow" style={{ marginLeft: 10 }} onClick={() => addItem(item)}>&#10095;</div>
+                <div style={{ marginLeft: 'auto', cursor: 'pointer' }} onClick={() => clearItem(item)}>&#10005;</div>
+            </div>
         </div>
     </div>
 );
 
-export default CartItem;
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CartItem);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,42 +6,46 @@ import { createStructuredSelector } from 'reselect';
 import { auth } from '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component';
 import CardDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCartHidden, selectCartTotal } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
+const Header = ({ currentUser, hidden, total }) => {
+  return (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
       </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        {
+          currentUser ?
+            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+            :
+            <Link className='option' to='/signin'>SIGN IN</Link>
+        }
+        <CartIcon />
+        <div style={{ flex: '0 1 60px', paddingTop: '3px', textAlign: 'center' }}>${total}</div>
+      </div>
       {
-        currentUser ?
-          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-          :
-          <Link className='option' to='/signin'>SIGN IN</Link>
+        hidden ? null : <CardDropdown />
       }
-      <CartIcon />
     </div>
-    {
-      hidden ? null : <CardDropdown />
-    }
-  </div>
-);
+  )
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  hidden: selectCartHidden
+  hidden: selectCartHidden,
+  total: selectCartTotal
 });
 
 export default connect(mapStateToProps)(Header);
